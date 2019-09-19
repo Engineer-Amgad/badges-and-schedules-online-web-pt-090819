6daffae9aa76bf45dcb7f01a6ebd959d0e6067ba
Amgad Malik <amgadmalik@yahoo.com>
2019-09-19 00:57:07 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,11 @@
 # Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end 
+
+ 
+def batch_badge_creator(array)
+  badge_array = []
+  array.each {|element| array << badge_maker(element)}
+  badge_array
+end 
