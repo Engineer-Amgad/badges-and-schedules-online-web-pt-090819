6dcf5bec9c9fbf39bed20a8d36aa8bcdb4571073
Amgad Malik <amgadmalik@yahoo.com>
2019-09-19 01:00:39 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -6,6 +6,10 @@ end
  
 def batch_badge_creator(array)
   badge_array = []
-  array.each {|element| array << badge_maker(element)}
+  array.each {|element| badge_array << badge_maker(element)}
   badge_array
+end 
+
+def assign_rooms(array)
+  array.each_with_index
 end 
